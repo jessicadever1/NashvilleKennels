8d6a0539fe3f642f02c7db195c4b636cd5d395b8
jessicadever1 <jessicadever1@gmail.com>
2021-01-26 11:01:47 -0600

    adding Animal Details Btn

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,11 +1,16 @@
 import React from "react"
 import "./Animal.css"
+import { Link } from "react-router-dom"
 
-export const AnimalCard = ({ animal, customer, location }) => (
-    <section className="animal">
-        <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__breed">{animal.breed}</div>
-        <div className="location__name">Location: {animal.location.name}</div>
-        <div className="customer__name">Customer: {customer.name}</div>
-    </section>
-)
+export const AnimalCard = ({ animal }) => {
+    return (
+        <section className="animal">
+            <h3 className="animal__name">
+                <Link to={`/animals/detail/${animal.id}`}>
+                    {animal.name}
+                </Link>
+            </h3>
+            <div className="animal__breed">{animal.breed}</div>
+        </section>
+    )
+}
