ce7cbcea8f5c48af95d7e8e0cb486e673dbf5049
jessicadever1 <jessicadever1@gmail.com>
2021-01-27 11:44:26 -0600

    changed display of locations

--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { AnimalList } from "../animal/AnimalList"
 import "./Location.css"
 
 export const LocationCard = ({ location }) => (
     <section className="location">
         <h3 className="location__name">{location.name}</h3>
-        <address className="location__address">{location.address}</address>
+        <div className="location__employeeCount">{location.employees.length} employees</div>
+        <div className="location__animalCount">{location.animals.length} animals</div>
     </section>
 )
+
+
+//<address className="location__address">{location.address}</address>
--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -3,16 +3,22 @@ import { LocationContext } from "./LocationProvider"
 import { LocationCard } from "./LocationCard"
 import "./Location.css"
 import { useHistory } from "react-router-dom"
+import { EmployeeContext } from "../employee/EmployeeProvider"
+import { AnimalContext } from "../animal/AnimalProvider"
 
 export const LocationList = () => {
   // This state changes when `getLocations()` is invoked below
     const { locations, getLocations } = useContext(LocationContext)
+    // const [ employees, getEmployees ] = useContext(EmployeeContext)
+    // const [ animals, getAnimals ] = useContext(AnimalContext)
+
     const history = useHistory()
     //useEffect - reach out to the world for something
     useEffect(() => {
         console.log("LocationList: useEffect - getLocations")
         getLocations()
-
+        // .then(getEmployees)
+        // .then(getAnimals)
     }, [])
 
 
@@ -25,7 +31,9 @@ export const LocationList = () => {
                 {console.log("LocationList: Render", locations)}
                 {
                     locations.map(location => {
-                    return <LocationCard key={location.id} location={location} />
+                    // const copyOfEmployeeList = [ ...employees ]
+                    // const copyOfAnimalList = [ ...animals ]
+                    return <LocationCard key={location.id} location={location}  />
                     })
                 }
         </div>
